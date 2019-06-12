0907b6b60a989e62dc3f4a5d0e57ceae7ab072e1
Kacper Biedka <kacper.biedka.00@gmail.com>
2019-06-12 15:47:23 +0200

    Made the MovieList responsive

--- client/src/components/MovieList/MovieList.js
+++ client/src/components/MovieList/MovieList.js
@@ -63,7 +63,22 @@ class MovieList extends Component {
       {
         breakpoint: 1200,
         settings: {
-          slidesToShow: 3
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       }
     ]
