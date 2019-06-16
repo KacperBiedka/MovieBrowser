b826f1c8c9d1f569ea063b0898547d66e36b4d69
Kacper Biedka <kacper.biedka.00@gmail.com>
2019-06-16 17:14:56 +0200

    Made the MovieDetails component responsive

--- client/src/components/LoadingScreen/LoadingScreen.js
+++ client/src/components/LoadingScreen/LoadingScreen.js
@@ -9,16 +9,8 @@ const LoadingScreen = props => {
         className={classes.mainLoadingDiv}
         style={{ display: props.display }}
       >
-        <div className={classes.skCubeGrid}>
-          <div className={classes.skCube + " " + classes.skCube1} />
-          <div className={classes.skCube + " " + classes.skCube2} />
-          <div className={classes.skCube + " " + classes.skCube3} />
-          <div className={classes.skCube + " " + classes.skCube4} />
-          <div className={classes.skCube + " " + classes.skCube5} />
-          <div className={classes.skCube + " " + classes.skCube6} />
-          <div className={classes.skCube + " " + classes.skCube7} />
-          <div className={classes.skCube + " " + classes.skCube8} />
-          <div className={classes.skCube + " " + classes.skCube9} />
+        <div className={classes.loadingContainer}>
+          <div className={classes.loadingDiv} />
         </div>
       </div>
     </Fade>
--- client/src/components/MovieDetails/MovieDetails.js
+++ client/src/components/MovieDetails/MovieDetails.js
@@ -7,13 +7,19 @@ import classes from "./MovieDetails.module.sass";
 class MovieDetails extends Component {
   state = {
     movieDetailsClosed: false,
-    displayContent: false
+    displayContent: false,
+    mobile: false
   };
 
   componentDidMount = () => {
     this.setState({
       movieDetailsClosed: true
     });
+    if (window.innerWidth < 1000) {
+      this.setState({
+        mobile: true
+      });
+    }
   };
 
   closeMovieDetails = () => {
@@ -29,15 +35,101 @@ class MovieDetails extends Component {
   render() {
     return (
       <Slide down when={this.state.movieDetailsClosed}>
-        {!this.props.loading ? (
-          <div
-            style={{
-              background: `url(https://image.tmdb.org/t/p/original/${
-                this.props.movieDetails.backdrop_path
-              })`
-            }}
-            className={classes.movieDetailsMainDiv}
-          >
+        {!this.state.mobile ? (
+          !this.props.loading ? (
+            <div
+              style={{
+                background: `url(https://image.tmdb.org/t/p/original/${
+                  this.props.movieDetails.backdrop_path
+                })`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat"
+              }}
+              className={classes.movieDetailsMainDiv}
+            >
+              <div className={classes.opacityLayerDiv}>
+                <div
+                  className={
+                    "animated fadeInDown fast " +
+                    classes.movieDetailsContainerDiv
+                  }
+                >
+                  <div className={classes.chevronDiv}>
+                    <i
+                      onClick={this.closeMovieDetails}
+                      className={classes.chevronIcon + " material-icons"}
+                    >
+                      chevron_left
+                    </i>
+                  </div>
+                  <div className={classes.titleSectionDiv}>
+                    <div className={classes.movieTitleDiv}>
+                      <h5 className={classes.movieTitleHeader}>
+                        {this.props.movieDetails.title}
+                      </h5>
+                    </div>
+                    <div className={classes.movieDescribtionDiv}>
+                      <p className={classes.movieDescribtionText}>
+                        {this.props.movieDetails.overview}
+                      </p>
+                    </div>
+                  </div>
+                  <div className={classes.infoSectionDiv}>
+                    <div className={classes.movieInfoDiv}>
+                      <ul>
+                        <li>
+                          Release Date: {this.props.movieDetails.release_date}
+                        </li>
+                        <li>
+                          Production:{" "}
+                          {this.props.movieDetails.production_companies[0].name}
+                        </li>
+                        <li>
+                          Runtime:{" "}
+                          {parseInt(this.props.movieDetails.runtime / 60)} hours{" "}
+                          {this.props.movieDetails.runtime -
+                            parseInt(this.props.movieDetails.runtime / 60) *
+                              60}{" "}
+                          minutes
+                        </li>
+                        <li>
+                          Avarage rating: {this.props.movieDetails.vote_average}
+                        </li>
+                        <li>Budget: {this.props.movieDetails.budget}</li>
+                        <li>
+                          Revenue:{" "}
+                          {this.props.movieDetails.revenue === 0
+                            ? "No data available yet"
+                            : this.props.movieDetails.revenue}
+                        </li>
+                        <li>
+                          Genres:{" "}
+                          {this.props.movieDetails.genres.map(
+                            (genre, index) => {
+                              if (
+                                index ===
+                                this.props.movieDetails.genres.length - 1
+                              ) {
+                                return genre.name;
+                              } else {
+                                return genre.name + ", ";
+                              }
+                            }
+                          )}
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className={classes.loadingContainer}>
+              <div className={classes.loadingDiv} />
+            </div>
+          )
+        ) : !this.props.loading ? (
+          <div className={classes.movieDetailsMainDiv}>
             <div className={classes.opacityLayerDiv}>
               <div
                 className={
